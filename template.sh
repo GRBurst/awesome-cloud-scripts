5942b62b51a18c21f8f0a98e8c55783f7e190610
GRBurst <julius.elias@priceloop.ai>
2022-08-18 15:09:17 +0200

    chore: Enhance template with help message
    Signed-off-by: GRBurst <julius.elias@priceloop.ai>

--- template.sh
+++ template.sh
@@ -1,10 +1,22 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash --pure --keep ENV1 --keep ENV2 -p hello
+#! nix-shell -i bash
 #! nix-shell --pure
 #! nix-shell --keep ENV1 --keep ENV2
 #! nix-shell -p hello
 
-(
+set -Eeuo pipefail
+
+help() (
+    echo "${0##*/}"
+    echo "Explain what this script does and exit."
+    echo "Describes the nix-shell parameter and provides template for scripts."
+    exit 1
+)
+
+self() (
+    if [[ $# -ge 1 ]] && [[ "$1" == "help" ]]; then
+        help
+    fi
     echo "nix-shell"
     echo "      -i: provides interpreter, here bash."
     echo "  --pure: only packeges provided by -p are available + environment is cleaned."
@@ -14,3 +26,5 @@
     echo "The parameters will be merged"
     hello
 )
+
+self $@
